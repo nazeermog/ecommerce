0654013f92519719e86c45a93db5143e5effadf4
nazeer_mog <nazeermog7@gmail.com>
2025-07-21 18:36:07 +0300

    edit

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\AuthController;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+*/  
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
